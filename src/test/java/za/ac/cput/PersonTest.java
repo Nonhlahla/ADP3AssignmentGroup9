e57d71732d0992493dff0d68b4f4da7303d8460c
Jason Jaftha <jsnjaftha3@gmail.com>
2021-03-31 16:41:51 +0200

    Create Test class to test methods in the Person class

---
+++ src/test/java/za/ac/cput/PersonTest.java
@@ -0,0 +1,27 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    Author: Jason Jaftha
+    Student No: 217009301
+    Description: Test Class
+*/
+
+class PersonTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
