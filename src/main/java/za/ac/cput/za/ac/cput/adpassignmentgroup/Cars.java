d1fceceabf6be794422dbc8ef672af77c91763b8
Nonhlahla <nhlaa.nh@gmail>
2021-03-31 21:03:00 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/za/ac/cput/adpassignmentgroup/Cars.java
@@ -0,0 +1,12 @@
+package za.ac.cput.za.ac.cput.adpassignmentgroup;
+
+/**
+ * This is a simple car Manufacturer program
+ *
+ * @ author Nonhlahla Hlungwani 218160658
+ * 31 March 2021
+ */
+public class Cars {
+
+    }
+}
